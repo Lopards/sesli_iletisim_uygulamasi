0b2a63ecc9080904709c5c540159796005572cd2
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-09-05 14:01:29 +0300

    Update web_kayit.py
    şifreye hashleme ve tuzllama getirildi.
    kayit tarihi eklendi.
    login sayfası eklendi -> 500 hatası veriyor.

--- web_kayit.py
+++ web_kayit.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template, request
-import mysql.connector
+from flask import Flask, render_template, request,session
+from flask_mysqldb import MySQL
+import hashlib
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 
-# MySQL veritabanı bağlantısı
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Osmaniye12!",
-    database="rise_together"
-)
+# MySQL veritabanı bağlantı ayarları
+app.config['MYSQL_HOST'] = 'emirhan.mysql.pythonanywhere-services.com'
+app.config['MYSQL_USER'] = 'emirhan'
+app.config['MYSQL_PASSWORD'] = 'XXX'
+app.config['MYSQL_DB'] = 'emirhan$default'
 
-cursor = db.cursor()
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -25,25 +26,51 @@ def anasayfa():
 def about():
     return render_template("about.html")
 
-@app.route('/register.html')
-def register_page():
-    return render_template("register.html")
-
-
-
-@app.route('/register', methods=['POST'])
+@app.route('/register.html', methods=['GET', 'POST'])
 def register():
-    name = request.form['name']
-    email = request.form['email']
-    password = request.form['password']
+    if request.method == 'POST':
+        name = request.form.get('name')
+        password = request.form.get('password')
 
-    # Verileri MySQL veritabanına ekleme
-    query = "INSERT INTO kullanici_verileri (kullanici_ad, kullanici_sifre, kayit_zamani) VALUES (%s, %s, %s)"
-    values = (name, email, password)
-    cursor.execute(query, values)
-    db.commit()
+        # Tuz oluştur
+        salt = os.urandom(16)
 
-    return "Kayıt başarıyla tamamlandı!"
+        # Şifre ile tuzu birleştir
+        salted_password = salt + password.encode('utf-8')
 
-"""if __name__ == '__main__':
-    app.run(debug=True)"""
+        # Hashleme işlemi
+        hashed_password = hashlib.sha256(salted_password).hexdigest()
+
+
+        current_time = datetime.now() #kayit zamanı için
+        cursor = mysql.connection.cursor()
+        sorgu = "INSERT INTO veriler VALUES(%s,%s,%s,%s)"
+        cursor.execute(sorgu, (name, password,hashed_password,current_time))
+        mysql.connection.commit()
+        cursor.close()
+        kayit_basarili = True  # Kayıt başarılı oldu
+        return render_template("login.html", kullanici_ad=name, sifre=password,kayit_basarili=kayit_basarili)
+    else:
+        return render_template("register.html")
+
+@app.route('/login.html', methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST':
+        username = request.form['name']
+        password = request.form['password']
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM veriler WHERE kullanici_ad = % s AND sifre = % s', (username, password ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('login.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+            return render_template('index.html', msg = msg)
+
+    else:
+        return render_template('index.html', msg = msg)
