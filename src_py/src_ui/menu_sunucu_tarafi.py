181b0d91d33e8ebc24b229a4c8dd3ccac8064083
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-11-12 17:23:13 +0300

    menuler arası bilgiler artık korunuyor

--- src_py/src_ui/menu_sunucu_tarafi.py
+++ src_py/src_ui/menu_sunucu_tarafi.py
@@ -1,90 +1,72 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QVBoxLayout, QWidget
 from src_py.server_erkek_pyqt5 import server_erkek_page
 from src_py.server_kadin_pyqt5 import server_kadin_page
 from src_py.sunucu_pyqt5 import Sunucu_page
+
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.initUI()
-
-        self.server = server_erkek_page()
-       
+        self.menus = {}
 
     def initUI(self):
         self.setWindowTitle("Doktor - Menü")
         self.setGeometry(100, 100, 400, 300)
 
-        # Menü oluşturma
         menubar = self.menuBar()
         menu = menubar.addMenu("Seçenekler")
 
-        #Burada menu seçeneklerini oluşturuyoruz
         ses_islemleri = QAction("ses işlemleri", self)
-        ses_islemleri.triggered.connect(self.ses_islemleri)
+        ses_islemleri.triggered.connect(lambda: self.show_menu(ses_islemleri))
         menu.addAction(ses_islemleri)
 
         interkom_e = QAction("interkom (erkek)", self)
-        interkom_e.triggered.connect(self.interkom_e)
+        interkom_e.triggered.connect(lambda: self.show_menu(interkom_e))
         menu.addAction(interkom_e)
 
         interkom_k = QAction("interkom (kadın)", self)
-        interkom_k.triggered.connect(self.interkom_k)
+        interkom_k.triggered.connect(lambda: self.show_menu(interkom_k))
         menu.addAction(interkom_k)
 
-        
-
-
-        # İçerik göstermek için bir widget oluştur
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
         self.layout = QVBoxLayout(self.central_widget)
 
-        
-        sunucu = Sunucu_page()
-        self.layout.addWidget(sunucu)
+    def show_menu(self, menu_action):
+        # Menüyü ismine göre al
+        menu_name = menu_action.text()
+        menu = self.menus.get(menu_name)
 
-    def interkom_e(self):
-        # Mevcut pencerenin içeriğini temizle
-        for i in reversed(range(self.layout.count())):
-            widget = self.layout.itemAt(i).widget()
-            if widget:
-                widget.deleteLater()
-    # server_erkek_page sınıfından bir nesne oluştur
-        server_erkek = server_erkek_page()
-        
-        # Oluşturulan nesneyi mevcut pencerenin içeriği olarak ayarla
-        self.layout.addWidget(server_erkek)
+        # Eğer mevcut menü varsa göster, yoksa oluştur
+        if not menu:
+            if menu_name == "ses işlemleri":
+                menu = Sunucu_page()
+            elif menu_name == "interkom (erkek)":
+                menu = server_erkek_page()
+            elif menu_name == "interkom (kadın)":
+                menu = server_kadin_page()
 
-    def interkom_k(self):
-        # Mevcut pencerenin içeriğini temizle
-        for i in reversed(range(self.layout.count())):
-            widget = self.layout.itemAt(i).widget()
-            if widget:
-                widget.deleteLater()
+            self.layout.addWidget(menu)
+            self.menus[menu_name] = menu
 
-    # server_erkek_page sınıfından bir nesne oluştur
-        server_kadin = server_kadin_page()
-        
-        # Oluşturulan nesneyi mevcut pencerenin içeriği olarak ayarla
-        self.layout.addWidget(server_kadin)
+        # Diğer menüleri gizle
+        for existing_menu_name, existing_menu in self.menus.items():
+            if existing_menu_name != menu_name:
+                existing_menu.hide()
 
-    def ses_islemleri(self):
-        # Mevcut pencerenin içeriğini temizle
-        for i in reversed(range(self.layout.count())):
-            widget = self.layout.itemAt(i).widget()
-            if widget:
-                widget.deleteLater()
+        # Mevcut menüyü göster
+        menu.show()
 
-    # server_erkek_page sınıfından bir nesne oluştur
-        sunucu = Sunucu_page()
-        
-        # Oluşturulan nesneyi mevcut pencerenin içeriği olarak ayarla
-        self.layout.addWidget(sunucu)
-"""if __name__ == "__main__":
-    app = QApplication(sys.argv)
+if __name__ == "__main__":
+    app = QApplication([])
     window = MyWindow()
     window.show()
-    sys.exit(app.exec_())
-"""
+    app.exec_()
+
+
+"""if __name__ == "__main__":
+    app = QApplication([])
+    window = MyWindow()
+    window.show()
+    app.exec_()"""
