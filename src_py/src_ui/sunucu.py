bd4d07f86c4626fd31df258f84e60a0c9bda2a40
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-11-19 18:39:30 +0300

    sunucu.py eklendi

--- src_py/src_ui/sunucu.py
+++ src_py/src_ui/sunucu.py
@@ -16,11 +16,42 @@ class Ui_Dialog(object):
         Dialog.setObjectName("Dialog")
         Dialog.resize(695, 513)
         Dialog.setMinimumSize(QtCore.QSize(694, 479))
+        Dialog.setStyleSheet("color:white;\n"
+"background: #404040;")
         self.pushButton_DosyaSec = QtWidgets.QPushButton(Dialog)
         self.pushButton_DosyaSec.setGeometry(QtCore.QRect(30, 40, 93, 31))
+        self.pushButton_DosyaSec.setStyleSheet("/* Normal durum için */\n"
+"QPushButton {\n"
+"    background-color: #95A5A6; /* İlk renk */\n"
+"    color: #ffffff; /* Metin rengi */\n"
+"    border-radius:15px;\n"
+"}\n"
+"\n"
+"/* Üzerine gelindiğinde */\n"
+"QPushButton:hover {\n"
+"    background-color: #0d730d; /* İkinci renk */\n"
+"    \n"
+"}\n"
+"\n"
+"/* Tıklandığında */\n"
+"QPushButton:pressed {\n"
+"    background-color: #ffcc00; /* Üçüncü renk */\n"
+"    color: #000000; /* Tıklandığında metin rengini değiştirin, örneğin siyah yapın */\n"
+"}\n"
+"\n"
+"/* Tıklandıktan sonraki durum için (örneğin, bir toggle düğmesi) */\n"
+"QPushButton:checked {\n"
+"    background-color: #9932CC; /* Dördüncü renk */\n"
+"    color: #ffffff; /* Tıklandıktan sonraki durumda metin rengini değiştirin */\n"
+"}")
         self.pushButton_DosyaSec.setObjectName("pushButton_DosyaSec")
         self.lineEdit_dosyaAdi = QtWidgets.QLineEdit(Dialog)
         self.lineEdit_dosyaAdi.setGeometry(QtCore.QRect(160, 40, 401, 31))
+        self.lineEdit_dosyaAdi.setStyleSheet("QComboBox {\n"
+"    border: 2px solid #67b26f; /* Kenarlık rengi ve kalınlığı */\n"
+"    border-radius: 5px; /* Kenarlık köşe yuvarlama */\n"
+"}\n"
+"")
         self.lineEdit_dosyaAdi.setObjectName("lineEdit_dosyaAdi")
         self.radioButton_kadin = QtWidgets.QRadioButton(Dialog)
         self.radioButton_kadin.setGeometry(QtCore.QRect(40, 120, 95, 20))
@@ -33,33 +64,187 @@ class Ui_Dialog(object):
         self.radioButton_Cocuk.setObjectName("radioButton_Cocuk")
         self.groupBox_Metin = QtWidgets.QGroupBox(Dialog)
         self.groupBox_Metin.setGeometry(QtCore.QRect(380, 90, 271, 271))
+        self.groupBox_Metin.setStyleSheet("background: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 #393B3B,stop:1 #121313);\n"
+"border-radius:20px;")
         self.groupBox_Metin.setObjectName("groupBox_Metin")
         self.pushButton_MetinKaydet = QtWidgets.QPushButton(self.groupBox_Metin)
-        self.pushButton_MetinKaydet.setGeometry(QtCore.QRect(90, 150, 93, 28))
+        self.pushButton_MetinKaydet.setGeometry(QtCore.QRect(90, 150, 93, 31))
+        self.pushButton_MetinKaydet.setStyleSheet("/* Normal durum için */\n"
+"QPushButton {\n"
+"    background-color: #95A5A6; /* İlk renk */\n"
+"    color: #ffffff; /* Metin rengi */\n"
+"    border-radius:15px;\n"
+"}\n"
+"\n"
+"/* Üzerine gelindiğinde */\n"
+"QPushButton:hover {\n"
+"    background-color: #0d730d; /* İkinci renk */\n"
+"    \n"
+"}\n"
+"\n"
+"/* Tıklandığında */\n"
+"QPushButton:pressed {\n"
+"    background-color: #ffcc00; /* Üçüncü renk */\n"
+"    color: #000000; /* Tıklandığında metin rengini değiştirin, örneğin siyah yapın */\n"
+"}\n"
+"\n"
+"/* Tıklandıktan sonraki durum için (örneğin, bir toggle düğmesi) */\n"
+"QPushButton:checked {\n"
+"    background-color: #9932CC; /* Dördüncü renk */\n"
+"    color: #ffffff; /* Tıklandıktan sonraki durumda metin rengini değiştirin */\n"
+"}")
         self.pushButton_MetinKaydet.setObjectName("pushButton_MetinKaydet")
         self.pushButton_MetiNOku = QtWidgets.QPushButton(self.groupBox_Metin)
-        self.pushButton_MetiNOku.setGeometry(QtCore.QRect(90, 190, 93, 28))
+        self.pushButton_MetiNOku.setGeometry(QtCore.QRect(90, 190, 93, 31))
+        self.pushButton_MetiNOku.setStyleSheet("/* Normal durum için */\n"
+"QPushButton {\n"
+"    background-color: #95A5A6; /* İlk renk */\n"
+"    color: #ffffff; /* Metin rengi */\n"
+"    border-radius:15px;\n"
+"}\n"
+"\n"
+"/* Üzerine gelindiğinde */\n"
+"QPushButton:hover {\n"
+"    background-color: #0d730d; /* İkinci renk */\n"
+"    \n"
+"}\n"
+"\n"
+"/* Tıklandığında */\n"
+"QPushButton:pressed {\n"
+"    background-color: #ffcc00; /* Üçüncü renk */\n"
+"    color: #000000; /* Tıklandığında metin rengini değiştirin, örneğin siyah yapın */\n"
+"}\n"
+"\n"
+"/* Tıklandıktan sonraki durum için (örneğin, bir toggle düğmesi) */\n"
+"QPushButton:checked {\n"
+"    background-color: #9932CC; /* Dördüncü renk */\n"
+"    color: #ffffff; /* Tıklandıktan sonraki durumda metin rengini değiştirin */\n"
+"}")
         self.pushButton_MetiNOku.setObjectName("pushButton_MetiNOku")
         self.textEdit = QtWidgets.QTextEdit(self.groupBox_Metin)
         self.textEdit.setGeometry(QtCore.QRect(20, 20, 231, 87))
+        self.textEdit.setStyleSheet("background: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 #E7E7E7,stop:1 #D9E6E2);\n"
+"color:black;\n"
+"border-radius:15px;")
         self.textEdit.setObjectName("textEdit")
         self.efekt_combobox = QtWidgets.QComboBox(self.groupBox_Metin)
         self.efekt_combobox.setGeometry(QtCore.QRect(90, 120, 101, 22))
+        self.efekt_combobox.setStyleSheet("QComboBox {\n"
+"    border: 2px solid #67b26f; /* Kenarlık rengi ve kalınlığı */\n"
+"    border-radius: 5px; /* Kenarlık köşe yuvarlama */\n"
+"}\n"
+"")
         self.efekt_combobox.setObjectName("efekt_combobox")
         self.groupBox_AnlikSes = QtWidgets.QGroupBox(Dialog)
         self.groupBox_AnlikSes.setGeometry(QtCore.QRect(30, 310, 261, 111))
         self.groupBox_AnlikSes.setObjectName("groupBox_AnlikSes")
         self.pushButton_AnlikErkek = QtWidgets.QPushButton(self.groupBox_AnlikSes)
-        self.pushButton_AnlikErkek.setGeometry(QtCore.QRect(80, 10, 93, 28))
+        self.pushButton_AnlikErkek.setGeometry(QtCore.QRect(80, 10, 93, 31))
+        self.pushButton_AnlikErkek.setStyleSheet("/* Normal durum için */\n"
+"QPushButton {\n"
+"    background-color: #95A5A6; /* İlk renk */\n"
+"    color: #ffffff; /* Metin rengi */\n"
+"    border-radius:15px;\n"
+"}\n"
+"\n"
+"/* Üzerine gelindiğinde */\n"
+"QPushButton:hover {\n"
+"    background-color: #0d730d; /* İkinci renk */\n"
+"    \n"
+"}\n"
+"\n"
+"/* Tıklandığında */\n"
+"QPushButton:pressed {\n"
+"    background-color: #ffcc00; /* Üçüncü renk */\n"
+"    color: #000000; /* Tıklandığında metin rengini değiştirin, örneğin siyah yapın */\n"
+"}\n"
+"\n"
+"/* Tıklandıktan sonraki durum için (örneğin, bir toggle düğmesi) */\n"
+"QPushButton:checked {\n"
+"    background-color: #9932CC; /* Dördüncü renk */\n"
+"    color: #ffffff; /* Tıklandıktan sonraki durumda metin rengini değiştirin */\n"
+"}")
         self.pushButton_AnlikErkek.setObjectName("pushButton_AnlikErkek")
         self.pushButton_AnlikKadin = QtWidgets.QPushButton(self.groupBox_AnlikSes)
-        self.pushButton_AnlikKadin.setGeometry(QtCore.QRect(80, 60, 93, 28))
+        self.pushButton_AnlikKadin.setGeometry(QtCore.QRect(80, 57, 93, 31))
+        self.pushButton_AnlikKadin.setStyleSheet("/* Normal durum için */\n"
+"QPushButton {\n"
+"    background-color: #95A5A6; /* İlk renk */\n"
+"    color: #ffffff; /* Metin rengi */\n"
+"    border-radius:15px;\n"
+"}\n"
+"\n"
+"/* Üzerine gelindiğinde */\n"
+"QPushButton:hover {\n"
+"    background-color: #0d730d; /* İkinci renk */\n"
+"    \n"
+"}\n"
+"\n"
+"/* Tıklandığında */\n"
+"QPushButton:pressed {\n"
+"    background-color: #ffcc00; /* Üçüncü renk */\n"
+"    color: #000000; /* Tıklandığında metin rengini değiştirin, örneğin siyah yapın */\n"
+"}\n"
+"\n"
+"/* Tıklandıktan sonraki durum için (örneğin, bir toggle düğmesi) */\n"
+"QPushButton:checked {\n"
+"    background-color: #9932CC; /* Dördüncü renk */\n"
+"    color: #ffffff; /* Tıklandıktan sonraki durumda metin rengini değiştirin */\n"
+"}")
         self.pushButton_AnlikKadin.setObjectName("pushButton_AnlikKadin")
         self.pushButton_SesKaydi = QtWidgets.QPushButton(Dialog)
-        self.pushButton_SesKaydi.setGeometry(QtCore.QRect(590, 40, 93, 28))
+        self.pushButton_SesKaydi.setGeometry(QtCore.QRect(590, 37, 93, 31))
+        self.pushButton_SesKaydi.setStyleSheet("/* Normal durum için */\n"
+"QPushButton {\n"
+"    background-color: #95A5A6; /* İlk renk */\n"
+"    color: #ffffff; /* Metin rengi */\n"
+"    border-radius:15px;\n"
+"}\n"
+"\n"
+"/* Üzerine gelindiğinde */\n"
+"QPushButton:hover {\n"
+"    background-color: #0d730d; /* İkinci renk */\n"
+"    \n"
+"}\n"
+"\n"
+"/* Tıklandığında */\n"
+"QPushButton:pressed {\n"
+"    background-color: #ffcc00; /* Üçüncü renk */\n"
+"    color: #000000; /* Tıklandığında metin rengini değiştirin, örneğin siyah yapın */\n"
+"}\n"
+"\n"
+"/* Tıklandıktan sonraki durum için (örneğin, bir toggle düğmesi) */\n"
+"QPushButton:checked {\n"
+"    background-color: #9932CC; /* Dördüncü renk */\n"
+"    color: #ffffff; /* Tıklandıktan sonraki durumda metin rengini değiştirin */\n"
+"}")
         self.pushButton_SesKaydi.setObjectName("pushButton_SesKaydi")
         self.efekt_uygula = QtWidgets.QPushButton(Dialog)
-        self.efekt_uygula.setGeometry(QtCore.QRect(190, 260, 93, 28))
+        self.efekt_uygula.setGeometry(QtCore.QRect(190, 257, 93, 31))
+        self.efekt_uygula.setStyleSheet("/* Normal durum için */\n"
+"QPushButton {\n"
+"    background-color: #95A5A6; /* İlk renk */\n"
+"    color: #ffffff; /* Metin rengi */\n"
+"    border-radius:15px;\n"
+"}\n"
+"\n"
+"/* Üzerine gelindiğinde */\n"
+"QPushButton:hover {\n"
+"    background-color: #0d730d; /* İkinci renk */\n"
+"    \n"
+"}\n"
+"\n"
+"/* Tıklandığında */\n"
+"QPushButton:pressed {\n"
+"    background-color: #ffcc00; /* Üçüncü renk */\n"
+"    color: #000000; /* Tıklandığında metin rengini değiştirin, örneğin siyah yapın */\n"
+"}\n"
+"\n"
+"/* Tıklandıktan sonraki durum için (örneğin, bir toggle düğmesi) */\n"
+"QPushButton:checked {\n"
+"    background-color: #9932CC; /* Dördüncü renk */\n"
+"    color: #ffffff; /* Tıklandıktan sonraki durumda metin rengini değiştirin */\n"
+"}")
         self.efekt_uygula.setObjectName("efekt_uygula")
         self.radioButton_yasli_adam = QtWidgets.QRadioButton(Dialog)
         self.radioButton_yasli_adam.setGeometry(QtCore.QRect(40, 260, 121, 20))
