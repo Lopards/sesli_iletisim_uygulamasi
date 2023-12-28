1b2f6a836f300ffd13a8c75f250f1e253caa3287
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2023-12-29 01:16:32 +0300

    kod class içine alındı, connect ve disconnect olayları düzenlendi
    artık odadan çıkan kişinin bilgilerine ulaşılabiliyor.

--- sesli iletişim flask/web.py
+++ sesli iletişim flask/web.py
@@ -1,220 +1,181 @@
-from flask import Flask, render_template, request, session, redirect, url_for, send_file
-from flask_socketio import join_room, leave_room, send, SocketIO, emit
-import random
-from string import ascii_uppercase
+from flask import Flask, render_template, session, redirect, request
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
 import base64
+from flask_session import Session
 
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "hjhjsdahhds"
-app.config["JSON_AS_ASCII"] = False
+class ChatApp:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.app.config["SECRET_KEY"] = "hjhjsdahhds"
+        self.app.config["JSON_AS_ASCII"] = False
+        self.socketio = SocketIO(self.app)
+        # Flask-Session konfigürasyonu
+        self.app.config[
+            "SESSION_TYPE"
+        ] = "filesystem"  # Örnek bir depolama tipi, siz kendi projenize uygun birini seçebilirsiniz
+        Session(self.app)
 
-socketio = SocketIO(app)
-
-rooms = {}
-users_in_rooms = {}
-room_s = ""  # bu değişkeni eklemeden önce ses verileri bütün odalara gidityordu. artık sesler kullanıcının olduğu odaya gidecek.
-
-
-def generate_unique_code(length):  # Benzersiz oda kodu oluşturmaya yarar
-    while True:
-        code = ""
-        for _ in range(length):
-            code += random.choice(ascii_uppercase)
-
-        if code not in rooms:
-            break
-
-    return code
+        self.rooms = {}
+        self.users_in_rooms = {}
+        self.room_s = {}
+        self.connected_users= {}
+        self.setup_routes()
 
 
-@app.route(
-    "/", methods=["POST", "GET"]
-)  # Eğer talep bir "POST" talebi ise, kullanıcının girdiği isim (name) ve oda kodu (code) bilgilerini alır.
-def home():
-    if request.method == "POST":
-        name = request.form.get("name")
-        code = request.form.get("code")
-        join = request.form.get("join", False)
-        create = request.form.get("create", False)
+    def setup_routes(self):
+        @self.app.route("/")
+        def index():
+            return render_template("index.html")
+        
+        @self.socketio.on("baglan")
+        def baglan(data):
+            print("veri:",data)
+            name = data.get("name")  # Name ve room bilgilerini auth içinden alır
+            room = data.get("room")
+            print("name print ediliyor:",name)
 
-        if not name:
-            return render_template(
-                "home.html", error="Please enter a name.", code=code, name=name
+            if not room or not name:
+                print("room yok veya name yok")
+                return
+            if room not in self.rooms:  # eğer alınan room kodu rooms içinde yoksa fonksiyondan çıkar
+                print("room yok")
+                leave_room(room)
+                return
+            
+            join_room(room)
+            
+            # Bağlı kullanıcıların bilgilerini sakla
+            
+
+
+            send({"name": name, "message": "has entered the room"})
+            self.rooms[room]["members"].append(name)
+            self.users_in_rooms[room].append(name)
+            print("self.users.in.room [room]pritn etmek ben",self.users_in_rooms[room])
+            print(f"{name} joined room {room}")
+
+
+        @self.socketio.on("connect")
+        def connect(auth):  # Kullanıcıların sohbet odasına bağlanmasını sağlayar aut = kimlik bilgileri
+            print("baglanıldi")
+            
+
+
+        @self.socketio.on("disconnect")
+        def disconnect():
+            print("Kullanıcı odadan çıkıverdi: {}".format(request.sid))
+            user_data = self.connected_users.get(request.sid)
+            print("disccenet tarafı",user_data.get("name"))
+            # request.sid'ye bağlı olan kullanıcının bilgilerini al
+            if user_data:
+                name = user_data.get("name")
+                room = user_data.get("room")
+                print(name, "odadan çıktı")
+
+                if room in self.rooms:
+                    self.rooms[room]["members"].remove(name)
+                    self.users_in_rooms[room].remove(name)
+                    print(len(self.users_in_rooms[room]))
+                    if room in self.rooms and len(self.rooms[room]["members"]) <= 0:
+                        del self.rooms[room]
+
+                    self.socketio.emit("user_left", {"name": name, "room": room}, to=room)
+                    print(f"{name} has left the room {room}")
+
+
+
+        @self.socketio.on("create_room")
+        def handle_create_room(data):
+  
+            name = data["name"]
+            code = data["code"]
+
+            if code not in self.rooms:
+                self.rooms[code] = {"members": [name], "messages": []}
+                self.users_in_rooms[code] = [name]
+                print(self.users_in_rooms[code],"123")
+                print(f"Oda oluşturuldu. Adı: {name}, Kodu: {code}")
+            elif code in self.rooms:
+                print("Bu oda kodu zaten kullanılıyor.")
+
+            join_room(code)  # odaya katılındı
+
+            self.connected_users[request.sid] = {"name": name, "room": code}
+            self.socketio.emit("create_room", {"name": name, "code": code}, to=code)
+
+
+
+
+        @self.socketio.on("file_upload")
+        def handle_file_upload(data):
+            file_name = data["file_name"]
+            file_data_base64 = data["file_data"]
+
+            # Base64 veriyi çöz
+            file_data = base64.b64decode(file_data_base64)
+
+            # Dosyayı kaydet
+            with open(file_name, "wb") as file:
+                file.write(file_data)
+            file_data_encoded = base64.b64encode(file_data).decode("utf-8")
+            print(f"Received file: {file_name}")
+            self.socketio.emit(
+                "file_uploaded", {"filename": file_name, "file_data": file_data_encoded}
             )
+            # (Kodunuzun geri kalanı burada)
 
-        if join != False and not code:
-            return render_template(
-                "home.html", error="Please enter a room code.", code=code, name=name
-            )
+        @self.socketio.on("audio_data")
+        def audio_data(data1):
+            emit("data1", data1, to=self.room_s, broadcast=True)
+            # (Kodunuzun geri kalanı burada)
 
-        room = code
-        if create != False:
-            room = generate_unique_code(5)
-            rooms[room] = {"members": [name], "messages": []}
-            users_in_rooms[room] = [name]
-        elif code not in rooms:
-            return render_template(
-                "home.html", error="Room does not exist.", code=code, name=name
-            )
-        """else:
-            rooms[room]["members"].append(name)
-            users_in_rooms[room].append(name)"""
+        @self.socketio.on("audio_data2")
+        def audio_data2(data2):
+            emit("data2", data2, to=self.room_s, broadcast=True)
+            # (Kodunuzun geri kalanı burada)
 
-        session["room"] = room
-        session["name"] = name
-        return redirect(url_for("room"))
+        @self.socketio.on("output_device_list")
+        def output_device_list(List):
+            print(self.users_in_rooms)
+            print(self.rooms)
+            print(self.room_s,"room_s")
+            emit("liste", List, to=self.room_s)
 
-    return render_template("home.html")
+        @self.socketio.on("output_device_index")
+        def output_device_list(index):
+            emit("index", index, to=self.room_s)
+            # (Kodunuzun geri kalanı burada)
 
+        @self.socketio.on("message")
+        def message(data):
+            room = session.get("room") or data["room"]
+            name = session.get("name") or data["name"]
+            message = data["data"]
 
-@app.route("/room", methods=["GET", "POST"])
-def room():
-    room = session.get("room")
-    if room is None or session.get("name") is None or room not in rooms:
-        return redirect(url_for("home"))
+            efekt = data.get("efekt")
+            key = data.get("key")  # Anahtarı al
 
-    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+            if room not in self.rooms:
+                print("heyo")
+                return
 
-
-@socketio.on("message")
-def message(data):  # Kullanıcıların sohbet odasında mesaj göndermesini sağlar
-    room = session.get("room") or data["room"]
-    name = session.get("name") or data["name"]
-    message = data["data"]
-
-    efekt = data.get("efekt")
-    key = data.get("key")  # Anahtarı al
-
-    if room not in rooms:
-        return
-
-    content = {
-        "name": name,
-        "message": message,
-        "efekt": efekt,
-        "key": key,  # Anahtarı ekle
-    }
-    send(content, to=room)
-    rooms[room]["messages"].append(content)
-    print(f"{name} said: {message}")
+            content = {
+                "name": name,
+                "message": message,
+                "efekt": efekt,
+                "key": key,  # Anahtarı ekle
+            }
+            send(content, to=room)
+            self.rooms[room]["messages"].append(content)
+            print(f"{name} said: {message}")
 
 
-@socketio.on("connect")
-def connect(
-    auth,
-):  # Kullanıcıların sohbet odasına bağlanmasını sağlayar aut = kimlik bilgileri
-    print(auth)
-    if (
-        not auth
-    ):  # ğer auth bilgisi yoksa veya eksikse, "Auth verisi eksik veya hatalı." şeklinde bir hata mesajı yazdırır ve fonksiyondan çıkar.
-        print("Auth verisi eksik veya hatalı.")
-        return
-
-    name = auth.get("name")  # Name ve room bilgilerini auth içinden alır
-    room = auth.get("room")
-
-    if not room or not name:
-        print("room yok veya name yok")
-        return
-    if room not in rooms:  # eğer alınan room kodu rooms içinde yoksa fonksiyondan çıkar
-        print("room yok")
-        leave_room(room)
-        return
-
-    join_room(room)
-    send({"name": name, "message": "has entered the room"}, to=room)
-    rooms[room]["members"].append(name)
-    users_in_rooms[room].append(name)
-    print(f"{name} joined room {room}")
 
 
-def disconnect():
-    room = session.get("room")
-    name = session.get("name")
-
-    if room in rooms:
-        rooms[room]["members"].remove(name)
-        users_in_rooms[room].remove(name)
-        if len(rooms[room]["members"]) <= 0:
-            del rooms[room]
-
-    send({"name": name, "message": "has left the room"}, to=room)
-    print(f"{name} has left the room {room}")
-
-
-@socketio.on("create_room")
-def handle_create_room(
-    data,
-):  # kullanıcıların oda oluşturmasını sağlar .data içined oda sahibi ismi(name) ve oda kodu vardır
-    name = data["name"]
-    code = data["code"]
-    global room_s
-    room_s = code  # sesin hangi odaya gideceği belirlendi
-
-    if code not in rooms:
-        rooms[code] = {"members": [name], "messages": []}
-        users_in_rooms[code] = [name]
-        print(f"Oda oluşturuldu. Adı: {name}, Kodu: {code}")
-    else:
-        print("Bu oda kodu zaten kullanılıyor.")
-    join_room(room_s)  # odaya katılındı
-
-    send("message", {"name": name, "code": code}, room=request.sid)
-
-
-@socketio.on("take_rooms")  # oda bilgilerini paylaşmak için konuldu, şu anda kullanılmıyor
-def take_rooms():
-    for room_name, room_data in rooms.items():
-        members = room_data["members"]
-        print(f"Üyeleri: {', '.join(members)}")
-        emit("members_data", members)
-
-
-@socketio.on("file_upload")
-def handle_file_upload(data):# doktor tarafından öğrenci tarafına dosya paylaşımı yapar. (Öğrenci tarafında sıkıntı olabilir tekrar bak)
-    file_name = data["file_name"]
-    file_data_base64 = data["file_data"]
-
-    # Base64 veriyi çöz
-    file_data = base64.b64decode(file_data_base64)
-
-    # Dosyayı kaydet
-    with open(file_name, "wb") as file:
-        file.write(file_data)
-    file_data_encoded = base64.b64encode(file_data).decode("utf-8")
-    print(f"Received file: {file_name}")
-    socketio.emit(
-        "file_uploaded", {"filename": file_name, "file_data": file_data_encoded}
-    )
-
-
-@socketio.on("audio_data") #ses iletişimi için; doktor tarafından öğrenciye
-def audio_data(data1):
-    emit("data1", data1, to=room_s, broadcast=True)
-
-
-@socketio.on("audio_data2")  #ses iletişimi için; öğrenci tarafından doktora
-def audio_data2(data2):
-    emit("data2", data2, to=room_s, broadcast=True)
-
-
-@socketio.on("output_device_list") # hoparlör listesini öğrenci tarafından doktor tarafına göndermek için
-def output_device_list(List):
-    emit("liste", List, to=room_s)
-
-@socketio.on("output_device_index") # öğrenci pc 'nin indexini gönderen metod
-def output_device_list(index):
-    emit("index", index, to=room_s)
-
-@socketio.on("screenShot")  # ekran görüntüsünü paylaşmak için, şu anda aktif değil.
-def screenShot(screenShot):
-    room = session.get("room")
-    print("ekran görüntüsü geldi")
-    emit("screenShot", screenShot, to=room)  # emit ile paylaş
-
-    print("ekran görüntüsü gönderildi")
+        if __name__ == "__main__":
+            self.socketio.run(self.app, host="192.168.1.45", debug=True)
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="YOUR_IP_ADRESS", debug=True)
+    chat_app = ChatApp()
