6bb8ab7953f2d7f272dd1fa8e9e9b5468b5f9c25
Emirhan  Erdem <101428835+Lopards@users.noreply.github.com>
2024-01-13 23:29:28 +0300

    main_work düzenlendi

--- main_work.py
+++ main_work.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QApplication
-from  src_py.secim_ekran_pyqt5 import login_page
+from src_py.kayit_yeri_pyqt5 import kayit
 #seçim ekranını çalıştırır.
 app = QApplication([])
-pencere = login_page()
+pencere = kayit()
 pencere.show()
-app.exec_()
+app.exec_() 
